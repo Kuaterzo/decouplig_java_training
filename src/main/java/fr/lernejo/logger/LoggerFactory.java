aba2e0318851a546bb3f24edcd0b2e1d57e990ef
Bryan <bryanchidiac@hotmail.fr>
2021-12-01 13:42:36 +0100

    Fin Part 2

---
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -0,0 +1,12 @@
+package fr.lernejo.logger;
+
+public class LoggerFactory {
+
+    public static Logger getLogger(String name){
+
+        Logger logger = new ConsoleLogger();
+
+        return logger;
+    }
+
+}
